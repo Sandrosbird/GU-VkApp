714e75667b6ef295d6b6e2e9e34d0eb590c59ea1
Emil Mescheryakov <emil.sam12@gmail.com>
2020-08-09 21:59:45 +0300

    Создать синглтон для хранения текущей сессии

---
+++ VKAppForGB/ViewController/MyFriendsTableViewController.swift
@@ -0,0 +1,78 @@
+//
+//  MyFriendsController.swift
+//  VKAppForGB
+//
+//  Created by Emil Mescheryakov on 29.07.2020.
+//  Copyright © 2020 Emil Mescheryakov. All rights reserved.
+//
+
+import UIKit
+
+class MyFriendsTableViewController: UITableViewController {
+    
+    var friendsArray = FriendsFactory.generateFriends()
+    var sortedFriends: [User] {
+        get {
+            return FriendsFactory.inAlphabetOrder(usersArray: friendsArray)
+        }
+        set { }
+    }
+    var sectionNames: [String] {
+        get {
+            var firstWordsArray = [String]()
+            for friend in sortedFriends {
+                if !firstWordsArray.contains(String(friend.name.first!)) {
+                    firstWordsArray.append(String(friend.name.first!))
+                }
+            }
+            
+            let sortedFirstWordsArray = firstWordsArray.sorted(by: { $0.lowercased() < $1.lowercased() })
+            return sortedFirstWordsArray
+        }
+        set { }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionNames.count
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return sectionNames
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionNames.remove(at: section)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        let indexPath = IndexPath.init()
+//        var numberOfRowsInSection = Int()
+//
+//        if  sectionNames[indexPath.row].contains(friendsArray[indexPath.row].name.first!) {
+//            numberOfRowsInSection += 1
+//        }
+//
+//        return numberOfRowsInSection
+        return sortedFriends.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyFriendsCell", for: indexPath) as! MyFriendsCell
+        let currentSectionName = sectionNames[indexPath.section]
+        let firstLetterOfName = String(sortedFriends[indexPath.row].name.first!)
+        if  firstLetterOfName == currentSectionName {
+            let friend = sortedFriends[indexPath.row]
+            cell.friendIcon.image = friend.avatar
+            cell.friendName.text = friend.name
+        }
+        
+        return cell
+    }
+    
+    //добавить header и секции для таблицы
+}
---
+++ VKAppForGB/ViewController/MyGroupsTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MyGroupsTableViewController.swift
+//  VKAppForGB
+//
+//  Created by Emil Mescheryakov on 29.07.2020.
+//  Copyright © 2020 Emil Mescheryakov. All rights reserved.
+//
+
+import UIKit
+
+class MyGroupsTableViewController: UITableViewController {
+    
+    @IBAction func addGroup(seque: UIStoryboardSegue) {
+        guard seque.identifier == "addGroup" else { return }
+        let allGroupsController = seque.source as! AllGroupsTableViewController
+        
+        if let indexPath = allGroupsController.tableView.indexPathForSelectedRow {
+            let group = allGroupsController.groupsArray[indexPath.row]
+            
+            if !groupsArray.contains(group){
+                groupsArray.append(group)
+                tableView.reloadData()
+            }
+        }
+    }
+    
+    var groupsArray = [Group]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groupsArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyGroupsCell", for: indexPath) as! MyGroupsTableViewCell
+        let group = groupsArray[indexPath.row]
+        cell.groupImage.image = group.groupAvatar
+        cell.groupName.text = group.name
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            groupsArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+}
