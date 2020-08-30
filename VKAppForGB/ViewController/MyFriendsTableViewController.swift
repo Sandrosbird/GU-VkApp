0cc513aa4f019b8d7251c1be7e3177c9dfda670f
Emil Mescheryakov <emil.sam12@gmail.com>
2020-08-30 22:26:34 +0300

    no message

--- VKAppForGB/ViewController/MyFriendsTableViewController.swift
+++ VKAppForGB/ViewController/MyFriendsTableViewController.swift
@@ -10,50 +10,60 @@ import UIKit
 import RealmSwift
 
 class MyFriendsTableViewController: UITableViewController {
-
+    
     @IBOutlet var searchTextField: UISearchBar?
+    
     var searchText: String { searchTextField?.text ?? "" }
     
-//    private var searchedFriends: Results<User>? {
-//        guard !searchText.isEmpty else { return friendsArray! }
-//
-////        return friendsArray?.filter(NSPredicate(format: "firstName CONTAINS[cd] %@", searchText))
-//        return friendsArray?.filter({ (friend) -> Bool in
-//            friend.firstName.contains(searchText)
-//        })
-//    }
     
     private let realmService = RealmService.shared
+    private var realmToken: NotificationToken?
+    
     
     var friendsArray: Results<User>? {
         let friends: Results<User>? = realmService?.getFromRealm()
         return friends
     }
     
-    
-    //make set from friends array
-//    lazy var sortedFriends = User.inAlphabetOrder(users: friendsArray)
-    
+    private var searchedFriends: Results<User>? {
+        guard !searchText.isEmpty else { return friendsArray! }
         
-//
-//    var sectionNames: [String] {
-//        get {
-//            sortedFriends.keys
-//        }
-//        set { }
-//    }
+        return friendsArray?.filter(NSPredicate(format: "firstName CONTAINS[cd] %@ OR lastName CONTAINS[cd] %@", searchText, searchText))
+    }
     
     private func loadFriends(completion: (() -> Void)? = nil) {
         guard friendsArray != nil else { return }
-            NetworkService.shared.friendsRequest() { [weak self] friends in
-                DispatchQueue.main.async {
-                    try? self?.realmService?.addManyObjects(objects: friends)
-                    self?.tableView.reloadData()
-                    print("данные из сети")
-                    completion? ()
-                }
+        NetworkService.shared.friendsRequest() { [weak self] friends in
+            DispatchQueue.main.async {
+                try? self?.realmService?.addManyObjects(objects: friends)
+                self?.tableView.reloadData()
+                //                    print("данные из сети")
+                completion? ()
             }
-       
+        }
+    }
+    
+    private func createRealmNotification() {
+        
+        realmToken = friendsArray?.observe { (changes: RealmCollectionChange) in
+            switch changes {
+            case .initial(_):
+                
+                self.tableView.reloadData()
+                
+            case let .update(_, deletions: deletions, insertions: insertions, modifications: modifications):
+                
+                self.tableView.beginUpdates()
+                self.tableView.insertRows(at: insertions.map({IndexPath(row: $0, section: 0)}), with: .right)
+                self.tableView.deleteRows(at: deletions.map({IndexPath(row: $0, section: 0)}), with: .left)
+                self.tableView.reloadRows(at: modifications.map({IndexPath(row: $0, section: 0)}), with: .fade)
+                self.tableView.endUpdates()
+                
+            case .error(let error):
+                
+                print(error)
+            }
+        }
         
     }
     
@@ -64,69 +74,77 @@ class MyFriendsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.refreshControl?.attributedTitle = NSAttributedString(string: "Pull down to refresh")
+        self.refreshControl?.addTarget(self, action: #selector(self.refreshTable(_:)), for: .valueChanged)
+        
         if let friendsArray = friendsArray, friendsArray.isEmpty {
             loadFriends()
+            createRealmNotification()
         }
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
-//        return sectionNames.count
+        //        return sectionNames.count
     }
-//
-//    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-//        return sectionNames
-//    }
-//
-//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        return sectionNames.remove(at: section)
-//    }
+    //
+    //    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+    //        return sectionNames
+    //    }
+    //
+    //    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    //        return sectionNames.remove(at: section)
+    //    }
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        let indexPath = IndexPath.init()
-//        var numberOfRowsInSection = Int()
-//
-//        if  sectionNames[indexPath.row].contains(friendsArray[indexPath.row].name.first!) {
-//            numberOfRowsInSection += 1
-//        }
-//
-//        return numberOfRowsInSection
-//        friendsArray = NetworkService.shared.friendsRequest()
+        //        let indexPath = IndexPath.init()
+        //        var numberOfRowsInSection = Int()
+        //
+        //        if  sectionNames[indexPath.row].contains(friendsArray[indexPath.row].name.first!) {
+        //            numberOfRowsInSection += 1
+        //        }
+        //
+        //        return numberOfRowsInSection
+        //        friendsArray = NetworkService.shared.friendsRequest()
         
-        return friendsArray?.count ?? 0
-//            sortedFriends.count
+        return searchedFriends?.count ?? 0
         
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyFriendsCell", for: indexPath) as! MyFriendsCell
-//        let currentSectionName = sectionNames[indexPath.section]
-//        let firstLetterOfName = String(sortedFriends[indexPath.row].firstName.first!)
-//        if  firstLetterOfName == currentSectionName {
-//            let friend = sortedFriends[indexPath.row]
-////            cell.friendIcon.image = friend.photo
-//            cell.friendName.text = friend.firstName+" "+friend.lastName
-//        }
-              
-        let friend = friendsArray?[indexPath.row]
+        //        let currentSectionName = sectionNames[indexPath.section]
+        //        let firstLetterOfName = String(sortedFriends[indexPath.row].firstName.first!)
+        //        if  firstLetterOfName == currentSectionName {
+        //            let friend = sortedFriends[indexPath.row]
+        ////            cell.friendIcon.image = friend.photo
+        //            cell.friendName.text = friend.firstName+" "+friend.lastName
+        //        }
+        
+        let friend = searchedFriends?[indexPath.row]
         let urlForAvatar = friend?.photo
         
         guard let url = URL(string: urlForAvatar ?? ""), let data = try? Data(contentsOf: url) else { return cell }
         
         cell.friendName.text = "\(friend?.firstName ?? "NO")  \(friend?.lastName ?? "Name")"
         cell.friendIcon.image = UIImage(data: data)
-
+        
         
         return cell
+    }
+    
+    @objc func refreshTable(_ sender: Any?) {
+        loadFriends()
+        print("refresh")
+        refreshControl?.endRefreshing()
     }
     
     
 }
 
-extension MyFriendsTableViewController {
-    static func inAlphabetOrder(usersArray: [User]) -> [User] {
-        let sortedFriends = usersArray.sorted(by: { $0.firstName.lowercased() < $1.firstName.lowercased() })
-        return sortedFriends
+extension MyFriendsTableViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        tableView.reloadData()
     }
 }
--- VKAppForGB/ViewController/MyGroupsTableViewController.swift
+++ VKAppForGB/ViewController/MyGroupsTableViewController.swift
@@ -11,12 +11,23 @@ import RealmSwift
 
 class MyGroupsTableViewController: UITableViewController {
     
+    @IBOutlet var searchTextField: UISearchBar?
+
     
     private let realmService = RealmService.shared
+    private var realmToken: NotificationToken?
+    
+    private var searchText: String { searchTextField?.text ?? "" }
     
     var groupsArray: Results<Group>? {
         let groups: Results<Group>? = realmService?.getFromRealm()
         return groups
+    }
+    
+    var searchedGroups: Results<Group>? {
+        guard !searchText.isEmpty else { return groupsArray! }
+        
+        return groupsArray?.filter(NSPredicate(format: "name CONTAINS[cd] %@", searchText))
     }
     
     private func loadGroups() {
@@ -30,12 +41,41 @@ class MyGroupsTableViewController: UITableViewController {
         }
     }
     
+    private func createRealmNotification() {
+        
+        realmToken = groupsArray?.observe { (changes: RealmCollectionChange) in
+            switch changes {
+            case .initial(_):
+                
+                self.tableView.reloadData()
+                
+            case let .update(_, deletions: deletions, insertions: insertions, modifications: modifications):
+                
+                self.tableView.beginUpdates()
+                self.tableView.insertRows(at: insertions.map({IndexPath(row: $0, section: 0)}), with: .right)
+                self.tableView.deleteRows(at: deletions.map({IndexPath(row: $0, section: 0)}), with: .left)
+                self.tableView.reloadRows(at: modifications.map({IndexPath(row: $0, section: 0)}), with: .fade)
+                self.tableView.endUpdates()
+                
+            case .error(let error):
+               
+                print(error)
+            }
+        }
+        
+    }
+
+    
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.refreshControl?.attributedTitle = NSAttributedString(string: "Pull down to refresh")
+        self.refreshControl?.addTarget(self, action: #selector(self.refreshTable(_:)), for: .valueChanged)
         
         if let groupsArray = groupsArray, groupsArray.isEmpty {
             loadGroups()
@@ -47,13 +87,13 @@ class MyGroupsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return groupsArray?.count ?? 0
+        return searchedGroups?.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyGroupsCell", for: indexPath) as! MyGroupsTableViewCell
         
-        let group = groupsArray?[indexPath.row]
+        let group = searchedGroups?[indexPath.row]
         let groupAvatarURL = group?.photo
         
         guard let url = URL(string: groupAvatarURL ?? ""), let data = try? Data(contentsOf: url) else { return cell }
@@ -64,6 +104,12 @@ class MyGroupsTableViewController: UITableViewController {
         return cell
     }
     
+    @objc func refreshTable(_ sender: Any?) {
+        loadGroups()
+        print("refresh")
+        refreshControl?.endRefreshing()
+    }
+    
 //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
 //        if editingStyle == .
 //            groupsArray.remove(at: indexPath.row)
@@ -71,4 +117,10 @@ class MyGroupsTableViewController: UITableViewController {
 //        }
 //    }
     
+}
+
+extension MyGroupsTableViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        tableView.reloadData()
+    }
 }
